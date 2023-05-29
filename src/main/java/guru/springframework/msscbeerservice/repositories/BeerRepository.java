41cdbbb2d7b3abbe4b40c9cdda427abba13f4cc6
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2023-05-29 20:33:20 +0300

    adding bootstrap

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -1,9 +1,11 @@
 package guru.springframework.msscbeerservice.repositories;
 
 import guru.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.Repository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends CrudRepository<Beer, UUID>, PagingAndSortingRepository<Beer,UUID> {
 }
